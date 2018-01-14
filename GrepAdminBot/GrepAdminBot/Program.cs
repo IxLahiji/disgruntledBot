e09228adff8d33cb96a52e690d3f059e96650c35
IxLahiji <ixabat16@gmail.com>
2018-01-14 03:36:09 -0600

    Added template for bot login

--- GrepAdminBot/GrepAdminBot/Program.cs
+++ GrepAdminBot/GrepAdminBot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using GrepAdminBot.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,8 +14,37 @@ namespace GrepAdminBot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private IConfigurationRoot _config;
+
+        public async Task StartAsync()
         {
+            var builder = new ConfigurationBuilder()    // Begin building the config file
+                .SetBasePath(AppContext.BaseDirectory)  // Specify the location of the config
+                .AddJsonFile("_config.json");           // Add the config file
+            _config = builder.Build();                  // Build the config file
+
+            var services = new ServiceCollection()      // Begin building the service provider
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig     // Add the discord client to the service provider
+                {
+                    LogLevel = LogSeverity.Verbose,
+                    MessageCacheSize = 1000             // Tell Discord.Net to cache 1000 messages per channel
+                }))
+                .AddSingleton(new CommandService(new CommandServiceConfig     // Add the command service to the service provider
+                {
+                    DefaultRunMode = RunMode.Async,     // Force all commands to run async
+                    LogLevel = LogSeverity.Verbose
+                }))
+                .AddSingleton<Startup>()
+                .AddSingleton(_config);
+
+            var provider = services.BuildServiceProvider();     // Create the service provider
+
+            await provider.GetRequiredService<Startup>().StartAsync();
+
+            await Task.Delay(-1);     // Prevent the application from closing
         }
     }
 }
