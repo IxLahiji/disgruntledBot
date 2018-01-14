13b54229883e517ea2c9241a29b5d5ec01941522
IxLahiji <ixabat16@gmail.com>
2018-01-14 05:10:47 -0600

    Added moderation module for mod commmands

---
+++ GrepAdminBot/GrepAdminBot/Modules/Moderation.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace GrepAdminBot.Modules
+{
+    [Name("Moderator")]
+    [RequireContext(ContextType.Guild)]
+    public class Moderation : ModuleBase<SocketCommandContext>
+    {
+        [Command("adminTest")]
+        [Summary("Checks to see if you're an administrator.")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task AdminTest()
+        {
+            await ReplyAsync($"You are an admin :smile:");
+        }
+
+        [Command("userRoles")]
+        [Summary("Returns the users Role")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task UserRoles([Remainder]SocketGuildUser user)
+        {
+            await ReplyAsync($"Roles are {user.Roles.ToString()}.");
+        }
+    }
+}
