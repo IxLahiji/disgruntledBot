15d728583ed412ecacc7726920a285d06d1b95e6
IxLahiji <ixabat16@gmail.com>
2018-01-16 04:59:36 -0600

    Added function for multiple flips

--- GrepAdminBot/GrepAdminBot/Modules/Public.cs
+++ GrepAdminBot/GrepAdminBot/Modules/Public.cs
@@ -17,11 +17,35 @@ namespace GrepAdminBot.Modules
             this.rand = rand;
         }
 
-        [Command("flip")]
+        [Command("flip"), Priority(0)]
         [Summary("Flips a coin, and provides the result in chat.")]
         public async Task Flip()
         {
             await ReplyAsync($"Coin flipped! Result is {Coin()}.");
+        }
+
+        [Command("flip"), Priority(1)]
+        [Summary("Flips multiple coins, based on a provided positive number, and provides the results in chat.")]
+        public async Task Flip([Remainder]int numFlips)
+        {
+            // Check to make sure number provided is greater than 1
+            if (numFlips > 0)
+            {
+                await ReplyAsync($"{numFlips} coins flipped! Results is {MultCoins(numFlips)}.");
+            }
+            else
+            {
+                await ReplyAsync("Please use a number greater than 0.");
+            }
+        }
+
+        private string MultCoins(int n)
+        {
+            // Put first coin result, this is allowed because n > 0
+            string results = Coin();
+            // Loop for rest of tosses starts at 1, to account for initial toss
+            for (int i = 1; i < n; i++) results += (", " + Coin());
+            return results;
         }
 
         private string Coin()
