29853128399a6bc8128619d9d0ea4cdca5099c90
IxLahiji <ixabat16@gmail.com>
2018-01-16 04:28:47 -0600

    Created empty class for public commands

---
+++ GrepAdminBot/GrepAdminBot/Modules/Public.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+using System;
+using System.Threading.Tasks;
+
+namespace GrepAdminBot.Modules
+{
+    [Name("Public")]
+    public class Public : ModuleBase<SocketCommandContext>
+    {
+        
+    }
+}
