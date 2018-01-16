80c99587391e1804b1d00d00ee0b1ab65232f194
IxLahiji <ixabat16@gmail.com>
2018-01-16 04:34:56 -0600

    Added coin toss function

--- GrepAdminBot/GrepAdminBot/Modules/Public.cs
+++ GrepAdminBot/GrepAdminBot/Modules/Public.cs
@@ -10,6 +10,30 @@ namespace GrepAdminBot.Modules
     [Name("Public")]
     public class Public : ModuleBase<SocketCommandContext>
     {
-        
+        private readonly Random rand;
+
+        public Public(Random rand)
+        {
+            this.rand = rand;
+        }
+
+        [Command("flip")]
+        [Summary("Flips a coin, and provides the result in chat.")]
+        public async Task Flip()
+        {
+            await ReplyAsync($"Coin flipped! Result is {Coin()}.");
+        }
+
+        private string Coin()
+        {
+            if ((rand.Next() % 2) == 0)
+            {
+                return "HEADS";
+            }
+            else
+            {
+                return "TAILS";
+            }
+        }
     }
 }
